8583bf903acb4302f6708ea221f5f4376308bd5c
peunj04 <peunj04@gmail.com>
2020-06-20 16:10:19 +0900

    DIAL 인텐트 예제

--- app/src/main/java/com/example/androidintent_20200409/MainActivity.kt
+++ app/src/main/java/com/example/androidintent_20200409/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.androidintent_20200409
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+    }
+
+    fun setupEvents() {
+        dialBtn.setOnClickListener {
+
+            //어느 화면으로 갈지 직접 명시. => Uri를 이용해서 명시.
+            val myUri=Uri.parse("tel:01012341234")
+            val myIntent=Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+        }
     }
 }
